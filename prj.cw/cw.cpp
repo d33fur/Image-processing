0f1d5381d34d0a9b132530ca5de971594d063bfe
d33fur <d33fur@gmail.com>
2024-02-25 02:13:29 +0300

    lab02 fixes

---
+++ prj.cw/cw.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  
+  return 0;
+}
--- prj.lab/lab02/lab02.cpp
+++ prj.lab/lab02/lab02.cpp
@@ -7,97 +7,110 @@
 using namespace cv;
 using namespace std;
 
-void makeHistogram(const Mat& image, Mat& histImage) {
+Mat drawHistogram(const Mat& noisyImage) {
+  Mat histImage;
   vector<Mat> bgr_planes;
-  split(image, bgr_planes);
-  int histSize = 16;
+  split(noisyImage, bgr_planes);
+
+  int histSize = 256;
   float range[] = {0, 256};
   const float *histRange[] = {range};
   bool uniform = true, accumulate = false;
+
   Mat b_hist, g_hist, r_hist;
   calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate);
   calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate);
   calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate);
 
-  int hist_w = 512, hist_h = 400;
-  int bin_w = cvRound((double) hist_w / histSize);
-  histImage = Mat(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+  int w = 256;
+  int bin_w = cvRound((double) w / histSize);
+  histImage = Mat(w, w, CV_8UC3, Scalar(230, 230, 230));
   normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
   normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
   normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
 
   for (int i = 1; i < histSize; i++) {
     rectangle(histImage,
-      Point(bin_w * (i - 1), hist_h),
-      Point(bin_w * i, hist_h - cvRound(b_hist.at<float>(i - 1))),
-      Scalar(255, 255, 255)
-      // ,
-      // FILLED
-      );
+      Point(bin_w * (i - 1), w),
+      Point(bin_w * i, w - cvRound(b_hist.at<float>(i - 1))),
+      Scalar(0, 0, 0),
+      FILLED);
     rectangle(histImage,
-      Point(bin_w * (i - 1), hist_h),
-      Point(bin_w * i, hist_h - cvRound(g_hist.at<float>(i - 1))),
-      Scalar(255, 255, 255)      
-      // ,
-      // FILLED
-      );
+      Point(bin_w * (i - 1), w),
+      Point(bin_w * i, w - cvRound(g_hist.at<float>(i - 1))),
+      Scalar(0, 0, 0),
+      FILLED);
     rectangle(histImage,
-      Point(bin_w * (i - 1), hist_h),
-      Point(bin_w * i, hist_h - cvRound(r_hist.at<float>(i - 1))),
-      Scalar(255, 255, 255)
-      // ,
-      // FILLED
-      );
+      Point(bin_w * (i - 1), w),
+      Point(bin_w * i, w - cvRound(r_hist.at<float>(i - 1))),
+      Scalar(0, 0, 0),
+      FILLED);
   }
+
+  return histImage;
 }
 
-int main(int argc, char* argv[]) {
-  cv::CommandLineParser parser(argc, argv,
-    "{ w | 800 | side of a square }"
-    "{ c1 | 40 | color of big square }"
-    "{ c2 | 127 | color small square }"
-    "{ c3 | 220 | color of circle }");
-
-  auto w = parser.get<int>("w");
-  auto w1 = (w - sqrt(w * w * 2 / 3)) / 2;
-  auto w3 = sqrt(w * w / (3 * 3.14));
-  auto c1 = parser.get<int>("c1");
-  auto c2 = parser.get<int>("c2");
-  auto c3 = parser.get<int>("c3");
-
-  Mat originalImage(w, w, CV_8UC3, Scalar(c1, c1, c1));
-
-  rectangle(originalImage,
-    Point(w1, w1),
-    Point(w - w1, w - w1),
-    Scalar(c2, c2, c2),
-    FILLED,
-    LINE_8);
-
-  circle(originalImage,
-    Point(w / 2, w / 2),
-    w3,
-    Scalar(c3, c3, c3),
-    FILLED,
-    LINE_8);
-
-  Mat noise(originalImage.size(), CV_8UC3);
-  randn(noise, Scalar::all(0), Scalar::all(25));
+Mat generateNoiseImage(const Mat& image, const int& c) {
+  Mat noise(256, 256, CV_8UC3);
+  randn(noise, Scalar::all(0), Scalar::all(c));
   
   Mat noisyImage;
-  add(originalImage, noise, noisyImage);
+  add(image, noise, noisyImage);
 
-  Mat histImageOriginal;
-  makeHistogram(originalImage, histImageOriginal);
+  return noisyImage;
+}
 
-  Mat histImageNoisy;
-  makeHistogram(noisyImage, histImageNoisy);
+Mat generateHistograms(const Mat& image) {
+  Mat canvas = Mat::zeros(256 * 7, 256, CV_8UC3);
+  image.copyTo(canvas(Rect(0, 0, 256, 256)));
 
-  imshow("Original Image", originalImage);
-  imshow("Noisy Image", noisyImage);
-  imshow("Histogram of Original Image", histImageOriginal);
-  imshow("Histogram of Noisy Image", histImageNoisy);
+  int i = 1;
+  for(const auto& c : {3, 7, 15}) {
+    Mat noisyImage = generateNoiseImage(image, c);
+    noisyImage.copyTo(canvas(Rect(0, 256 * i++, 256, 256)));
+    drawHistogram(noisyImage).copyTo(canvas(Rect(0, 256 * i++, 256, 256)));
+  }
+  
+  return canvas;
+}
 
+Mat generateImage(const vector<int>& c) {
+  Mat image(256, 256, CV_8UC3, Scalar(c[0], c[0], c[0]));
+  auto p1 = (256-209) / 2;
+  auto p2 = 256 - p1;
+
+  rectangle(image,
+    Point(p1, p1),
+    Point(p2, p2),
+    Scalar(c[1], c[1], c[1]),
+    FILLED,
+    LINE_8);
+
+  circle(image,
+    Point(128, 128),
+    83,
+    Scalar(c[2], c[2], c[2]),
+    FILLED,
+    LINE_8);
+    
+  return generateHistograms(image);
+}
+
+int main() {
+  Mat canvas = Mat::zeros(256 * 7, 256 * 4, CV_8UC3);
+  vector<vector<int>> colors = {
+    {0, 127, 255},
+    {20, 127, 235},
+    {55, 127, 200},
+    {90, 127, 165}
+  };
+  
+  int i = 0;
+  for(const auto& c : colors) {
+    generateImage(c).copyTo(canvas(Rect(i++ * 256, 0, 256, 256 * 7)));
+  }
+
+  imshow("Image", canvas);
   waitKey(0);
 
   return 0;
